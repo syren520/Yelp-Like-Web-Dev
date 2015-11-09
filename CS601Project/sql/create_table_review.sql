b1c8108268754bba4817ae5f72af95b9878ac29f
syren520 <xuyurong@yahoo.com>
2015-11-08 23:58:49 -0800

    [project]remove sql files

--- CS601Project/sql/create_table_review.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE review (
-  reviewid INT NOT NULL AUTO_INCREMENT,
-  username VARCHAR(50) NOT NULL,
-  businessid VARCHAR(50) NOT NULL,
-  rating ENUM('1', '2', '3', '4', '5') NOT NULL,
-  description TEXT NOT NULL,
-  imagepath VARCHAR(100) NULL,
-  PRIMARY KEY (reviewid));
--- CS601Project/sql/create_table_user.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE user (
-  userid  INT NOT NULL AUTO_INCREMENT,
-  username VARCHAR(50) NOT NULL,
-  password VARCHAR(100) NOT NULL,
-  email VARCHAR(100) NOT NULL,
-  PRIMARY KEY (userid),
-  UNIQUE (username),
-  UNIQUE (email));
