426caeb13694ebd5e73297d5d2aa3ace42850d7b
syren520 <xuyurong@yahoo.com>
2015-11-08 23:47:38 -0800

    [project]Refactor layout style

--- CS601Project/webContent/JS/login.js
+++ CS601Project/webContent/JS/login.js
@@ -1,15 +1,15 @@
 $(document).ready(function(e){
-    $('#login-form-link').click(function(e) {
+    $('#btn-login-form').click(function(e) {
         $("#login-form").delay(100).fadeIn(100);
         $("#register-form").fadeOut(100);
-        $('#register-form-link').removeClass('active');
+        $('#btn-register-form').removeClass('active');
         $(this).addClass('active');
         e.preventDefault();
     });
-    $('#register-form-link').click(function(e) {
+    $('#btn-register-form').click(function(e) {
         $("#register-form").delay(100).fadeIn(100);
         $("#login-form").fadeOut(100);
-        $('#login-form-link').removeClass('active');
+        $('#btn-login-form').removeClass('active');
         $(this).addClass('active');
         e.preventDefault();
     });
