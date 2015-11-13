93fc71b710c03688b0cc3ca4b57351127261f522
syren520 <xuyurong@yahoo.com>
2015-11-12 22:23:27 -0800

    [project]modify method serve static files

--- CS601Project/src/Controller/BootStrap.java
+++ CS601Project/src/Controller/BootStrap.java
@@ -1,9 +1,10 @@
 package Controller;
 
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
 
 public class BootStrap {
 
@@ -12,15 +13,9 @@ public class BootStrap {
 	 */
 	public static void main(String[] args) throws Exception {	
 		//Set port number
-		Server server = new Server(8080);
+		Server server = new Server(11050);
 		// create a ServletHander to attach servlets
 		ServletContextHandler servhandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
-		//Make server could serve static files
-		servhandler.setContextPath("/");
-		DefaultServlet defaultServlet = new DefaultServlet();
-		ServletHolder holderPwd = new ServletHolder("default", defaultServlet);
-		holderPwd.setInitParameter("resourceBase", "./webContent/");
-		servhandler.addServlet(holderPwd, "/*");
 		
 		//Map server url
 		servhandler.addServlet(LoginServlet.class, "/login");
@@ -30,9 +25,19 @@ public class BootStrap {
 		servhandler.addServlet(RegisterUserServlet.class, "/registration");
 		servhandler.addServlet(ViewBusinessListServlet.class, "/viewBusinessList");
 		servhandler.addServlet(ViewSpecialBusinessListServlet.class, "/viewSpecialBusinessList");
+		//Make server could serve static files
+		//ResourceHandler is used to serve static files
+		ResourceHandler resource_handler = new ResourceHandler();
+		//Set the directory to true
+        resource_handler.setDirectoriesListed(true);
+        //Set static resources root is folder webContent
+        resource_handler.setResourceBase("webContent");
+        //Create a list to hold all handlers
+        HandlerList handlers = new HandlerList();
+        //Add handlers to this handler list
+        handlers.setHandlers(new Handler[] { resource_handler, servhandler });
 		// set the list of handlers for the server
-		server.setHandler(servhandler);
-
+        server.setHandler(handlers);
 		// start the server
 		server.start();
 		server.join();
